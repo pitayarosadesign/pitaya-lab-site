adaaf32b26397254e38f3a406376e5369011d374
Estephany Sugey Canedo <pitayarosa.design@gmail.com>
2026-07-14 15:42:42 -0600

    Mover meta tag de verificación Pinterest a layout default.vue con useHead

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,4 +16,11 @@
 <script setup>
 import NavBar from '~/components/NavBar.vue'
 import FooterSection from '~/components/FooterSection.vue'
+
+useHead({
+  meta: [
+    { name: 'p:domain_verify', content: 'd1f4d6f02a0cc071ee113b862fd17940' }
+  ]
+})
 </script>
+
